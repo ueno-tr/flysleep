1b1ad74b18c65f9c8f9bfa21c928c6f7505857d4
ueno-tr <t-ueno@umin.ac.jp>
2018-03-17 21:00:35 +0900

    delta sleep
    delta sleep

--- R/SDgraph.R
+++ R/SDgraph.R
@@ -10,7 +10,7 @@ SDgraph <- function(hrs) {
 
   ##グラフに出力
   library(ggplot2)
-  p <- ggplot(out, aes(x = time, y = mean,group=genotype, colour=genotype) ) + geom_line() + ylab("sleep (min/hr)")
+  p <- ggplot(out, aes(x = time, y = mean,group=genotype, colour=genotype) ) + geom_line() + ylab("delta sleep (min/hr)")
 
   errors <- aes(ymax = mean + SEM, ymin = mean - SEM)
   p <- p + geom_errorbar(errors, width = 0.2) + geom_point(size = 2)
